72129c6a5ad1e3c7bc7a2bcb2539d9c27069975d
evangeline24 <atinajara@gmail.com>
2022-01-30 21:43:40 -0500

    premio

--- concursoDeMascotasFX/src/main/java/ec/edu/espol/model/Premio.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/model/Premio.java
@@ -5,6 +5,7 @@
  */
 package ec.edu.espol.model;
 
+import ec.edu.espol.util.Busqueda;
 import ec.edu.espol.util.Util;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,25 +15,19 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
-/**
- *
- * @author Pibaque Ponce
- */
+
 public class Premio {
-    //Atributos
+    
     private int id;
     private int lugar;
-    private String descripcion;
-    private int idConcurso;
-    private Concurso j;
+    private String descripcion;    
+    private Concurso concurso;
     
-    //Constructor
-
-    public Premio(int id, int lugar, String descripcion, int idConcurso) {
+    public Premio(int id, int lugar, String descripcion, Concurso concurso) {
         this.id = id;
         this.lugar = lugar;
         this.descripcion = descripcion;
-        this.idConcurso = idConcurso;
+        this.concurso = concurso;
     }
 
     public int getId() {
@@ -59,73 +54,42 @@ public class Premio {
         this.descripcion = descripcion;
     }
 
-    public int getIdConcurso() {
-        return idConcurso;
+    public Concurso getConcurso() {
+        return concurso;
     }
 
-    public void setIdConcurso(int idConcurso) {
-        this.idConcurso = idConcurso;
+    public void setConcurso(Concurso concurso) {
+        this.concurso = concurso;
     }
 
-    public Concurso getJ() {
-        return j;
-    }
-
-    public void setJ(Concurso j) {
-        this.j = j;
-    }
-    
-    
-public static Premio nextPremio(Scanner sc){
-    
-        System.out.println("Ingrese descripcion concurso: ");
-        String descripcion = sc.next();
-        sc.nextLine();
-       sc.useDelimiter("\n");
-        System.out.println("Ingrese lugar del premio:");
-        int lugar= sc.nextInt();
-       // sc.useDelimiter("\n");
-       System.out.println("Ingrese el nombre del Concurso que pertenese el concurso:");
-       String nombreConcurso= sc.next();
-       int idMJ = 0;
-                ArrayList<Concurso> j = Concurso.readFile("concurso.txt");
-                 for(Concurso x: j){
-                      if(x.getNombre().equals(nombreConcurso)){
-                          idMJ = x.getId();
-                                         }
-                 }
-        return new Premio(Util.nextID("premios.txt"),lugar, descripcion, idMJ );
-          }
-    
-     public void saveFile(String nomfile){
+    public void saveFile(String nomfile){
         try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true))){
-           
-           pw.println(this.id+"|"+this.lugar+"|"+this.descripcion+"|"+this.idConcurso);
-       }
-       
-       catch(Exception e){
-           System.out.println(e.getMessage());
-       }
-   }
-
-      public static ArrayList<Premio> readFile(String nomfile){
-
-       ArrayList<Premio> premios = new ArrayList<>();
-       try(Scanner sc = new Scanner(new File(nomfile))){
-           while(sc.hasNextLine()){
-               String linea = sc.nextLine();
-               String[] tokens = linea.split("\\|");
-               Premio c = new Premio(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]),tokens[2],Integer.parseInt(tokens[3]));              
-               premios.add(c);
-           }
-           
-       }
-       catch(Exception e){
-           System.out.println(e.getMessage());
-       }
-       return premios; 
+            pw.println(this.id+"|"+this.lugar+"|"+this.descripcion+"|"+this.concurso.getId());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
-   
+    public static ArrayList<Premio> readFile(String nomfile){
+        ArrayList<Premio> premios = new ArrayList<>();
+        try(Scanner sc = new Scanner(new File(nomfile))){
+            while(sc.hasNextLine()){
+                String linea = sc.nextLine();
+                String[] tokens = linea.split("\\|");                
+                Premio c = new Premio(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]),tokens[2],Busqueda.buscarConcurso(Integer.parseInt(tokens[3])));              
+                premios.add(c);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return premios; 
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -139,21 +103,16 @@ public static Premio nextPremio(Scanner sc){
             return false;
         }
         final Premio other = (Premio) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (!Objects.equals(this.idConcurso, other.idConcurso)) {
-            return false;
-        }
-        return true;
-        
-        
+        return this.id == other.id;
     }
+
 
     @Override
     public String toString() {
-        return "Premio{" + "id=" + id + ", lugar=" + lugar + ", descripcion=" + descripcion + ", idConcurso=" + idConcurso + '}';
-    }
+        return "Premio{" + "id=" + id + ", lugar=" + lugar + ", descripcion=" + descripcion + ", idConcurso=" + concurso.getId() + '}';
+    } 
+    
+    
     
     
     
