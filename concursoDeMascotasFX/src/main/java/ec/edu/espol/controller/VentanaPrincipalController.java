318c2d3b124c0ade2b09f92d971ead8d3a9e48c2
wilsonpinzon <73978815+wilsonpinzon@users.noreply.github.com>
2022-01-30 20:53:21 -0500

    Update VentanaPrincipalController.java

--- concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaPrincipalController.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaPrincipalController.java
@@ -5,15 +5,23 @@
  */
 package ec.edu.espol.controller;
 
+import ec.edu.espol.concursodemascotasfx.App;
+import ec.edu.espol.model.Concurso;
+import ec.edu.espol.util.Util;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -21,32 +29,92 @@ import javafx.scene.control.ComboBox;
  * @author Pibaque Ponce
  */
 public class VentanaPrincipalController implements Initializable {
-
+    private VentanaPrincipalController vpc;
+    private Concurso concurso;
     @FXML
-    private ComboBox<?> comBx;
-
+    private BorderPane contenedor;
+    @FXML
+    private Label lblNombre;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList item = FXCollections.observableArrayList();
-        item.add("Registre Concurso");
-        item.add("Registre Criterios");
-        item.add("Registre Dueño");
-        item.add("Registre Evaluacion");
-        item.add("Registre Inscripcion");
-        item.add("Registre Mascota");
-        item.add("Registre Concurso");
-        
-        
-        
-       comBx.setItems(item);
+        vpc = this;
     }    
 
+    
+    public void recibirConcurso(Concurso concurso){
+        this.concurso = concurso;
+        this.lblNombre.setText(concurso.getNombre());
+    }
+
     @FXML
-    private void opciones(ActionEvent event) {
+    private void registrarDueno(ActionEvent event) {
         
     }
+
+    @FXML
+    private void registrarMascota(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarJurado(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarPremio(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarCriterio(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarEvaluacion(ActionEvent event) {
+    }
+
+    @FXML
+    private void mostrarInformacion(ActionEvent event) {
+    }
+
+    private void mostrarOriginal(){
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("ventanaPrincipal.fxml"));
+        try {            
+            Parent root = loader.load();
+            VentanaPrincipalController principal = loader.getController();                      
+            principal.recibirConcurso(concurso);
+            this.contenedor.setLeft(principal.contenedor.getLeft());
+            this.contenedor.setCenter(principal.contenedor.getCenter());
+        } catch (IOException ex) {
+            Util.alertaError("ERROR", ex.getMessage());
+        }
+    }
     
+    @FXML
+    private void regresar(ActionEvent event) {
+        try {            
+            App app = new App();
+            app.start(new Stage());
+            Stage myStage = (Stage) this.lblNombre.getScene().getWindow();
+            myStage.close();
+        } catch (IOException ex) {
+            Util.alertaError("ERROR", ex.getMessage());
+        }
+    }
+    
+    private void setearContenedor(String ventana){
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(ventana));
+        try {            
+            Parent root = loader.load();
+            VentanaPrincipalController principal = loader.getController();                      
+            principal.recibirConcurso(concurso);
+            this.contenedor.setLeft(principal.contenedor.getLeft());
+            this.contenedor.setCenter(principal.contenedor.getCenter());
+        } catch (IOException ex) {
+            Util.alertaError("ERROR", ex.getMessage());
+        }
+    }
 }
