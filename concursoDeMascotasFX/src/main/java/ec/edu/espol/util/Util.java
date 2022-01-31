28a859194cc09945ef39ba62e93edeb1d8a92752
evangeline24 <atinajara@gmail.com>
2022-01-30 20:46:00 -0500

    util

--- concursoDeMascotasFX/src/main/java/ec/edu/espol/util/Util.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/util/Util.java
@@ -7,6 +7,7 @@ package ec.edu.espol.util;
 
 import java.io.File;
 import java.util.Scanner;
+import javafx.scene.control.Alert;
 
 /**
  *
@@ -19,22 +20,31 @@ public class Util {
     // por lo tanto, no se van a permitir crear instancia de la clase Util
     private Util(){}
     
-    public static int nextID(String nomfile)
-    {
+    public static int nextID(String nomfile){
         int id = 0;
         try(Scanner sc = new Scanner(new File(nomfile)))
         {
-           while(sc.hasNextLine())
-           {
+           while(sc.hasNextLine()){
                String linea = sc.nextLine();
                String[] tokens = linea.split("\\|");
                id = Integer.parseInt(tokens[0]);
            }
         }
-        catch(Exception e)
-        {
+        catch(Exception e){
         }
         return id+1;
+    }
+    public static void alertaError(String encabezado, String mensaje){
+        Alert a = new Alert(Alert.AlertType.ERROR, encabezado);
+        a.setTitle(encabezado);
+        a.setContentText(mensaje);
+        a.show();
+    }
+    public static void alertaInfo(String encabezado, String mensaje){
+        Alert a = new Alert(Alert.AlertType.INFORMATION, encabezado);
+        a.setTitle(encabezado);
+        a.setContentText(mensaje);
+        a.show();
     }
     
 }
