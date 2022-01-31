43431f574ebd20b7970017d256ab92ac853fddbf
evangeline24 <atinajara@gmail.com>
2022-01-30 22:57:25 -0500

    cambios f2

--- concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaDueñoController.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaDueñoController.java
@@ -1,27 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.espol.controller;
 
+import ec.edu.espol.model.Dueño;
+import ec.edu.espol.util.Busqueda;
+import ec.edu.espol.util.Util;
+import ec.edu.espol.util.Validaciones;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 
-/**
- * FXML Controller class
- *
- * @author Pibaque Ponce
- */
+
 public class VentanaDueñoController implements Initializable {
 
+    @FXML
+    private BorderPane rootDueno;
+    @FXML
+    private TextField txtID;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private TextField txtDireccion;
+    @FXML
+    private TextField txtTelefono;
+    @FXML
+    private TextField txtEmail;
+
+    private VentanaDueñoController vdc;
+    private int id;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        vdc = this;
+        id = Util.nextID("Dueño.txt");
+        this.txtID.setText(String.valueOf(id));
     }    
+
+    public BorderPane getRootDueno() {
+        return rootDueno;
+    }
+
+    @FXML
+    private void registrar(ActionEvent event) {
+        if(validarCampos())
+            Util.alertaError("FALTA INFORMACION", "Debe completar todos los cmapos para registrar");
+        else if(!Validaciones.esEntero(this.txtTelefono.getText().trim()))
+            Util.alertaError("INFORMACION INCORRECTA", "El telefono solo puede contener valores numericos");
+        else if (Busqueda.buscarDueño(this.txtEmail.getText().trim())!=null)
+            Util.alertaError("DUEÑO YA REGISTRADO", "La persona que esta registrandose ya tiene un registro con el email ingresado");
+        else{
+            Dueño dueno = new Dueño(id, this.txtNombre.getText().trim(),this.txtApellido.getText().trim(), this.txtDireccion.getText().trim(),this.txtTelefono.getText().trim(), 
+                    this.txtEmail.getText().trim());
+            dueno.saveFile("Dueño.txt");
+            limpiarCampos();
+            Util.alertaInfo("REGISTRADO", "El dueño "+this.txtNombre.getText().trim()+" ha sido registrado");
+        }
+        
+    }
+    
+    private boolean validarCampos(){
+        return this.txtID.getText().trim().isEmpty() || this.txtNombre.getText().trim().isEmpty() || this.txtApellido.getText().trim().isEmpty() || 
+                this.txtDireccion.getText().trim().isEmpty() || this.txtTelefono.getText().trim().isEmpty() || this.txtEmail.getText().trim().isEmpty();
+    }
+    
+    private void limpiarCampos(){
+        id = Util.nextID("Dueño.txt");
+        this.txtID.setText(String.valueOf(id));
+        this.txtNombre.clear();
+        this.txtApellido.clear();
+        this.txtDireccion.clear();
+        this.txtTelefono.clear();
+        this.txtEmail.clear();
+    }
+    
     
 }
--- concursoDeMascotasFX/src/main/java/ec/edu/espol/util/Busqueda.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/util/Busqueda.java
@@ -2,6 +2,8 @@
 package ec.edu.espol.util;
 
 import ec.edu.espol.model.Concurso;
+import ec.edu.espol.model.Dueño;
+import ec.edu.espol.model.MiembroJurado;
 import ec.edu.espol.model.Premio;
 
 
@@ -11,10 +13,26 @@ public class Busqueda {
         return null;
     }
     
+    public static Dueño buscarDueño(String email){
+        for(Dueño d: Dueño.readFile("Dueño.txt")){
+            if(d.getEmail().equalsIgnoreCase(email))
+                return d;
+        }
+        return null;
+    }        
+    
     public static Concurso buscarConcurso(int idConcurso){
         for(Concurso c: Concurso.readFile("Concurso.txt")){
             if(c.getId() == idConcurso)
                 return c;
+        }
+        return null;
+    }
+    
+    public static MiembroJurado buscarJurado(int cedula){
+        for(MiembroJurado d: MiembroJurado.readFile("jurado.txt")){
+            if(d.getIdMJCedula() == cedula)
+                return d;
         }
         return null;
     }
