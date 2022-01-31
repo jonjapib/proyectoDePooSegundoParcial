8f5e7c4ff62ee712bd03cc99cd4e3433931e2a47
evangeline24 <atinajara@gmail.com>
2022-01-30 22:07:54 -0500

    inicioController

--- concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaInicioController.java
+++ concursoDeMascotasFX/src/main/java/ec/edu/espol/controller/VentanaInicioController.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.espol.controller;
+
 
 import ec.edu.espol.concursodemascotasfx.App;
 import ec.edu.espol.model.Concurso;
